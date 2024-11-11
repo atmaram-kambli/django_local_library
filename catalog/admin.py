a513d65d935418b90d1c32663595cc7c45b6113c
Atmaram Kambli <engr.atmaram.kambli22@gmail.com>
2024-11-11 13:51:27 +0530

    Added the language model and admin site configuration

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Author, Genre, Book, BookInstance, Language
 
-# Register your models here.
+# admin.site.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+admin.site.register(Author, AuthorAdmin)
+
+# admin.site.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_filter = ('status', 'due_back')
+admin.site.register(BookInstance, BookInstanceAdmin)
+
+# admin.site.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+admin.site.register(Book, BookAdmin)
+
+admin.site.register(Language)
+admin.site.register(Genre)
